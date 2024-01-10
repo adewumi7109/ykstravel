2894525bf353522f02c1671b33326e6eea7bd845
Adewumi <ogunsanyaadewumi01@gmail.com>
2024-01-10 15:22:30 +0100

    scale up

--- src/app/components/hero/Hero.tsx
+++ src/app/components/hero/Hero.tsx
@@ -11,12 +11,12 @@ function Hero() {
 
 <>
 <div className="relative">
-      {/* <img
+      <img
         src="/slideimages/s1.jpg"
         alt="Background"
-        className="w-full h-[80vh] md:h-[100vh]"
-      /> */}
-      <video src='/airplane_take_off.mp4' className="w-[100vw] h-[70vh] md:h-[80vh] object-cover" autoPlay loop muted/>
+        className="w-[100vw] h-[70vh] md:h-[80vh] object-cover"
+      />
+      {/* <video src='/airplane_take_off.mp4' className="w-[100vw] h-[70vh] md:h-[80vh] object-cover" autoPlay loop muted/> */}
       <div className="absolute inset-0 bg-black opacity-50"></div>
       <div className="absolute inset-0 flex px-4 items-center flex-col justify-center text-white  text-center font-bold">
         <h1 className=' text-2xl md:text-6xl text-cyan-200'>Embark on Seamless Journeys </h1>
