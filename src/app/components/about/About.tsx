694d1a85f28560dfa42ee984b79441b16093f41d
Adewumi <ogunsanyaadewumi01@gmail.com>
2024-01-10 20:42:22 +0100

    scale up

---
+++ src/app/components/about/About.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { FaPassport } from "react-icons/fa6";
+
+function About() {
+    return (
+        <>
+        <div >
+            <div className='p-4 md:p-10 shadow-lg pt-[3rem] pb-[3rem]'>
+                <h1 className='md:text-4xl text-3xl text-center text-cyan-500 '>About</h1>
+                <p className='mt-2'>
+                YKSTravel is a portal to extraordinary journeys designed for adventurers and explorers seeking seamless travel experiences. Our agency was founded on a profound passion for wanderlust and a steadfast commitment to delivering exceptional service. At YKSTravel , we are devoted to transforming your travel dreams into living, breathing realities.
+                </p>
+            </div>
+
+        </div>
+        </>
+    )
+}
+
+export default About
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image'
 import Navbar from './components/Navbar'
 import Hero from './components/hero/Hero'
+import Services from './components/services/Services'
+import About from './components/about/About'
 
 export default function Home() {
   return (
  <div>
   <Navbar/>
   <Hero/>
+  <About/>
+  <Services/>
 
  </div>
   )
