f029e10c83d16ee4f95331df9bcdb7ec7ff3b5a6
Adewumi <ogunsanyaadewumi01@gmail.com>
2024-01-11 14:09:04 +0100

    none

--- src/app/components/services/Services.tsx
+++ src/app/components/services/Services.tsx
@@ -12,10 +12,10 @@ function Services() {
         <div className="services bg-primary  p-4 md:p-10 ">
             <h1 className="md:text-4xl text-3xl text-center mt-[3rem] text-white">Our services</h1>
             <p className=" md:text-1xl text-center text-1xl mt-2 text-white">100% assurances on our services</p>
-            <div className="grid mt-[3rem] grid-cols-1 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-2 gap-8">
-                <div className="bg-[#fafdff]  rounded p-4 shadow-xl  " data-aos="fade-right">
+            <div className="grid mt-[3rem] grid-cols-1 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-3 gap-8">
+                <div className="bg-[white]  rounded p-4 shadow-xl  " data-aos="fade-right">
                     <h1 className="text-2xl text-primary font-bold">School Admission Processing:</h1>
-                    <ul className="mt-4 list-disc pl-4">
+                    <ul className="mt-4 list-disc text-[#696866] pl-4">
                         <li>Seamless assistance in processing school admissions in Canada, the UK, and Schengen countries.</li>
                         <li>Personalized guidance throughout the application process for a smooth and hassle-free experience.</li>
                         <li>Expert handling of documentation and admission requirements.</li>
@@ -29,9 +29,9 @@ function Services() {
                         <li>Step-by-step guidance to ensure a successful and timely application process.</li>
                     </ul>
                 </div>
-                <div className=" bg-[#fafdff] rounded p-4 shadow-xl " data-aos="fade-right">
+                <div className=" bg-[white] rounded p-4 shadow-xl " data-aos="fade-right">
                     <h1 className="text-2xl text-primary font-bold">Step-by-Step Application Process:</h1>
-                    <ul className="mt-4 list-disc pl-4">
+                    <ul className="mt-4 list-disc pl-4 text-[#696866] ">
                         <li>Detailed walkthrough of the entire application process to eliminate confusion.</li>
                         <li>Comprehensive assistance in filling out application forms accurately.</li>
                         <li>Ongoing support and updates to keep applicants informed at every stage.</li>
@@ -45,9 +45,9 @@ function Services() {
                         <li>Assistance in securing comprehensive insurance coverage for peace of mind during the entire stay.</li>
                     </ul>
                 </div>
-                <div className="bg-[#fafdff] rounded p-4 shadow-xl " data-aos="fade-right">
+                <div className="bg-[white] rounded p-4 shadow-xl " data-aos="fade-right">
                     <h1 className="text-2xl text-primary font-bold">Guidance on Accommodation/Hostel Booking:</h1>
-                    <ul className="mt-4 list-disc pl-4">
+                    <ul className="mt-4 list-disc pl-4 text-[#696866] ">
                         <li>Personalized recommendations and support in securing suitable accommodation or hostel.</li>
                         <li>Assistance in navigating the booking process to secure favorable rates.</li>
                         <li>Ensuring accommodation options align with the student&apos;s preferences and budget.</li>
