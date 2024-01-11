6c05bf7803e8e13ffff801e8dc774c7c93bb1f0f
Adewumi <ogunsanyaadewumi01@gmail.com>
2024-01-11 13:34:26 +0100

    scale up

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
 <div className='shadow-md w-full fixed top-0 left-0 z-10'>
 {/* bg-primary */}
-    <div className='md:flex items-center justify-between bg-[#f7faf8] py-4 md:px-10 px-4'>
+    <div className='md:flex items-center justify-between bg-[white] py-4 md:px-10 px-4'>
         <div className='font-bold text-2xl text-primary cursor-pointer flex items-center  '>
             YKSTRAVEL
         </div>
@@ -23,7 +23,7 @@ const Navbar = () => {
       {open ? <FaXmark/> :  <FaBars/>}
       
         </div>
-        <ul className={`md:flex md:items-center text-primary md:pb-0 pb-12 absolute md:static  md:z-auto z-[-1] left-0 w-full  md:w-auto md:pl-0 pl-4 transition-all duration-500 ease-in ${open ? ' top-[4.2rem] opacity-100 bg-[#f7faf8]': 'top-[-490px]'} md:opacity-100 `}>
+        <ul className={`md:flex md:items-center text-primary md:pb-0 pb-12 absolute md:static  md:z-auto z-[-1] left-0 w-full  md:w-auto md:pl-0 pl-4 transition-all duration-500 ease-in ${open ? ' top-[4.2rem] opacity-100 bg-white': 'top-[-490px]'} md:opacity-100 `}>
             {Links.map((link)=>(
               <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
                 <Link className=' hover:text-gray-400  duration-500' href={link.link}>{link.name}</Link>
--- src/app/components/services/Services.tsx
+++ src/app/components/services/Services.tsx
@@ -9,12 +9,12 @@ function Services() {
         Aos.init();
       }, [])
     return (
-        <div className="services  p-4 md:p-10 mt-[3rem]">
-            <h1 className="md:text-4xl text-3xl text-center text-primary">Our services</h1>
-            <p className=" md:text-1xl text-center text-1xl mt-2 ">100% assurances on our services</p>
+        <div className="services bg-primary  p-4 md:p-10 ">
+            <h1 className="md:text-4xl text-3xl text-center mt-[3rem] text-white">Our services</h1>
+            <p className=" md:text-1xl text-center text-1xl mt-2 text-white">100% assurances on our services</p>
             <div className="grid mt-[3rem] grid-cols-1 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-2 gap-8">
-                <div className="bg-primary  rounded p-4 shadow-xl text-white " data-aos="fade-right">
-                    <h1 className="text-2xl">School Admission Processing:</h1>
+                <div className="bg-[#fafdff]  rounded p-4 shadow-xl  " data-aos="fade-right">
+                    <h1 className="text-2xl text-primary font-bold">School Admission Processing:</h1>
                     <ul className="mt-4 list-disc pl-4">
                         <li>Seamless assistance in processing school admissions in Canada, the UK, and Schengen countries.</li>
                         <li>Personalized guidance throughout the application process for a smooth and hassle-free experience.</li>
@@ -22,15 +22,15 @@ function Services() {
                     </ul>
                 </div>
                 <div className="bg-primary rounded  p-4 shadow-xl text-white" data-aos="fade-left">
-                    <h1 className="text-2xl">Student Permit Application:</h1>
+                    <h1 className="text-2xl font-bold">Student Permit Application:</h1>
                     <ul className="mt-4 list-disc pl-4">
                         <li>Professional support for student permit applications tailored to the specific country&apos;s regulations.</li>
                         <li>Clear and concise information on eligibility criteria and required documentation.</li>
                         <li>Step-by-step guidance to ensure a successful and timely application process.</li>
                     </ul>
                 </div>
-                <div className="bg-primary rounded p-4 shadow-xl text-white" data-aos="fade-right">
-                    <h1 className="text-2xl">Step-by-Step Application Process:</h1>
+                <div className=" bg-[#fafdff] rounded p-4 shadow-xl " data-aos="fade-right">
+                    <h1 className="text-2xl text-primary font-bold">Step-by-Step Application Process:</h1>
                     <ul className="mt-4 list-disc pl-4">
                         <li>Detailed walkthrough of the entire application process to eliminate confusion.</li>
                         <li>Comprehensive assistance in filling out application forms accurately.</li>
@@ -38,15 +38,15 @@ function Services() {
                     </ul>
                 </div>
                 <div className="bg-primary rounded p-4 shadow-xl text-white" data-aos="fade-left">
-                    <h1 className="text-2xl">Guidance on Flight Booking, Pre-departure Briefing, and Insurance Booking:</h1>
+                    <h1 className="text-2xl font-bold">Guidance on Flight Booking, Pre-departure Briefing, and Insurance Booking:</h1>
                     <ul className="mt-4 list-disc pl-4">
                         <li>Expert advice on booking flights to ensure cost-effectiveness and convenience.</li>
                         <li>Informative pre-departure briefings covering essential details for a smooth transition.</li>
                         <li>Assistance in securing comprehensive insurance coverage for peace of mind during the entire stay.</li>
                     </ul>
                 </div>
-                <div className="bg-primary rounded p-4 shadow-xl text-white" data-aos="fade-right">
-                    <h1 className="text-2xl">Guidance on Accommodation/Hostel Booking:</h1>
+                <div className="bg-[#fafdff] rounded p-4 shadow-xl " data-aos="fade-right">
+                    <h1 className="text-2xl text-primary font-bold">Guidance on Accommodation/Hostel Booking:</h1>
                     <ul className="mt-4 list-disc pl-4">
                         <li>Personalized recommendations and support in securing suitable accommodation or hostel.</li>
                         <li>Assistance in navigating the booking process to secure favorable rates.</li>
