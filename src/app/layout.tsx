279ed86be816c5dfe68326394babf527804489aa
Adewumi <ogunsanyaadewumi01@gmail.com>
2024-01-29 14:09:59 +0100

    add indexing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'YKS Travel',
+  keywords: ["ykstravel", "YKSTravel", "YKSTRAVEL"],
+  authors: [{name:"ykstravel", url:"ykstravel.com.ng" }],
   description: 'Discover extraordinary travel experiences with YKSTravel – Your Key to Seamless Adventures. We offer personalized travel services, curated itineraries, and expert guidance. Explore breathtaking destinations, indulge in luxury accommodations, and create unforgettable memories. Your dream vacation starts with YKSTravel – where every journey is a bespoke adventure.',
 }
 
@@ -18,6 +20,8 @@ export default function RootLayout({
     <html lang="en">
       <head>
       <link rel="icon" href="/ykslogo.png" />
+      {/* <meta name="keywords" content="YKSTravel, ykstravel, YKSTRAVEL"/> */}
+
       </head>
       <body className={inter.className}>{children}</body>
     </html>
