6218a95b7b40073133d75a61494e432105d75b31
Adewumi <ogunsanyaadewumi01@gmail.com>
2024-01-29 14:48:18 +0100

    fixes icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'YKS Travel',
   keywords: ["ykstravel", "YKSTravel", "YKSTRAVEL", "yks travel", "yks"],
+  icons: '/ykslogo.png',
   authors: [{name:"ykstravel", url:"ykstravel.com.ng" }],
   description: 'Discover extraordinary travel experiences with YKSTravel – Your Key to Seamless Adventures. We offer personalized travel services, curated itineraries, and expert guidance. Explore breathtaking destinations, indulge in luxury accommodations, and create unforgettable memories. Your dream vacation starts with YKSTravel – where every journey is a bespoke adventure.',
 }
